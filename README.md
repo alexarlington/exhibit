7e6acad5f50fc77c7c0eec2f70104737dc5d2b7d
alexarlington <alexsarlington@gmail.com>
2023-02-02 20:56:21 +1300

    trying to make it work

---
+++ README.md
@@ -0,0 +1,32 @@
+const octopusRoute = require('./routes/octopus.js')
+
+// app.listen(port, err =>{
+//  if(err){
+//   return console.log("ERROR: ", err)
+//  }
+//  console.log(`listening on port ${port}`)
+// })
+app.listen(port)
+
+
+
+
+
+
+
+ROUTES:
+/ - homepage
+
+/octopus - main octopus image
+/octopus/1
+/octopus/2
+/octopus/3 - images
+
+/south-island - main map page
+/south-island/tasman
+/south-island/nelson
+/south-island/malborough
+/south-island/west-coast
+/south-island/canterbury
+/south-island/otago
+/south-island/southland
--- client/src/components/Octopus.jsx
+++ client/src/components/Octopus.jsx
@@ -1,35 +1,51 @@
 import React from "react";
+import { Component } from 'react'
  
-class Octopus extends React.Component {
- //  constructor(){
- //  super();
- //  this.state = {
- //   db: []
- //  }
- // }
- //  componentDidMount(){
+class Octopus extends Component {
 
- //   fetch('/db')
- //   .then(res => res.json())
- //   .then(db => {
- //    this.setState({db: db})
- //   })
- //  }
 
   render() {
    return <div>
-    <p>octopus jsx</p>
-    {/* {
-     this.state.db.map(db => {
-      <p>hi {db.Octopus} </p>
-     })
-    } */}
+    <p> 
+      hi from Octopus.jsx
+    </p>
    </div>;
 
   }
-
-
  }
  
+ export default Octopus
+ 
+ 
+ 
 
-export default Octopus
+
+ 
+//  constructor(){
+//   super();
+//   this.state = {
+//   octopus: []
+// }
+// }
+
+// componentDidMount(){
+// fetch('/octopus')
+// .then(res => res.json())
+// .then(octopus => {
+// this.setState({octopus: octopus})
+// })
+// }
+
+
+//  {
+//   this.state.db.map(db => {
+//    <p>hi {db.Octopus} </p>
+//   })
+//  }
+
+// {
+//   this.state.octopus.map(octopus => 
+//     <p key={octopus.id}>hi {octopus.english} </p>
+//   )
+// }
+
