83169c1f5cb45a4e0c99efeb9f3cad9c260b7b84
alexarlington <alexsarlington@gmail.com>
2023-02-08 14:25:20 +1300

    button finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
  
  
 function Button(props) {
-
+  const { pagename } = useParams()
   const url = props.page.data.exhibit
-  // const pageName = find.id
+
   const pageName = props.page.data.pages[0].id
-  const stringFind = pageName.includes("home")
-  // stringFind //true or false
-  console.log(pageName)
- 
- 
- 
- 
+  const stringFind = pagename.includes("-home") 
+
  return (
   <div>
    <button><Link to={`/`}>Choose Exhibit</Link></button>
